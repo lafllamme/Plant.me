2489ef0b0b3b6b16840092b7bb3c0425d0aa1132
Lafllamme <teke900@gmail.com>
2024-12-15 16:35:17 +0100

    feat: Add health check functionality with status logging
    Introduce a health check function to validate API endpoints using curl. Responses are logged with timestamp and color-coded status for easier debugging. European date format is implemented for consistency in logs.

--- health_check.sh
+++ health_check.sh
@@ -10,3 +10,41 @@ This endpoint, we use are the following:
 
 READ HERE: https://github.com/ollama/ollama/blob/main/docs/api.md
 '
+
+# Define color nodes
+
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# Function for time in european format
+get_time(){
+  date +"+%d-%m-%Y %H:%M:%S"
+}
+
+# Check given routes using curl
+health_check(){
+  # Define the endpoint
+  local endpoint="$1"
+  local url="http://localhost:11434/api/{$endpoint}"
+  local response
+
+  # Check the response
+  response=$(curl --write-out "%{http_code}" --silent --output /dev/null "$url")
+  local time
+  time=$(get_time)
+
+  # if response is 200, then we are good
+  if [ "$response" -eq 200 ]; then
+    echo -e "[{$GREEN}health_check{$NC}] (${endpoint} // $time) | ${GREEN}STATUS OK${NC} - HTTP Code: ${response}"
+  else
+    echo -e "[{$RED}health_check{$NC}] (${endpoint} // $time) | ${RED}STATUS FAIL${NC} - HTTP Code: ${response}"
+  fi
+
+  # Run health checks for the given endpoints
+  health_check "tags"
+  health_check "ps"
+}
+
+
